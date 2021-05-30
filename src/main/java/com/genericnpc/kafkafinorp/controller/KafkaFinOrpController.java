87ffc674b0f19b2c64a3f64d191cbb752f3df720
MythNPC <wrekk.game@gmail.com>
2021-05-30 14:34:04 +1000

    - Fix pom.xml
    - Batching POC finish

--- src/main/java/com/genericnpc/kafkafinorp/controller/KafkaFinOrpController.java
+++ src/main/java/com/genericnpc/kafkafinorp/controller/KafkaFinOrpController.java
@@ -1,21 +1,32 @@
 package com.genericnpc.kafkafinorp.controller;
 
 import com.example.Sensor;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.Flux;
 
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 @RestController
 public class KafkaFinOrpController {
     private Random random = new Random();
     BlockingQueue<Sensor> unbounded = new LinkedBlockingQueue<>();
+    private final EmitterProcessor<Message<Sensor>> processor = EmitterProcessor.create();
 
 
     private Sensor randomSensor() {
@@ -31,11 +42,23 @@ public class KafkaFinOrpController {
     @RequestMapping(value={"/message"})
     @ResponseBody
     public String getMessage(){
-        return "HelloWorld";
+        for(int i = 0; i < 5; i++) {
+            var a = MessageBuilder.withPayload(randomSensor()).build();
+            processor.onNext(a);
+        }
+        return "Sent mock event";
     }
 
     @Bean
-    public Supplier<Sensor> supplier() {
-        return () -> unbounded.poll();
+    public Supplier<Flux<Message<Sensor>>> supplier() {
+        return () -> processor;
     }
+
+    @Bean
+    public Consumer<Message<List<Sensor>>> consumer() {
+        return input -> {
+            System.out.println(input.getPayload().size());
+        };
+    }
+
 }
